794544868dcb79cdfb9f2f5c22f1c51ef1db07a2
adlesh <36427090+adlesh@users.noreply.github.com>
2020-02-22 16:23:12 -0500

    Initial Commit

---
+++ 100_doors.v
@@ -0,0 +1,15 @@
+const (
+        NbrDoors = 100
+)
+
+fn main() {
+        mut closed_doors := [true].repeat(NbrDoors)
+        for pass := 0; pass < NbrDoors; pass++ {
+                for door := 0; door < NbrDoors; door += pass + 1 {
+                        closed_doors[door] = !closed_doors[door]
+                }
+        }
+        for pass := 0; pass < NbrDoors; pass++ {
+                println('Door # $pass: ' + if closed_doors[pass] { 'Closed' } else { 'Open' })
+        }
+}
---
+++ pyth_means.v
@@ -0,0 +1,31 @@
+/*
+    Pythagorean Means 
+    Compute all three of the Pythagorean Means of the set of integers 1 through 10 (inclusive).
+*/
+import (
+    arrays
+    math
+)
+
+fn main() {
+    mut sum := 0.0
+    mut prod :=1.0
+    mut recip_sum := 0.0
+    n := 10.0
+    
+    for val in arrays.range<int>(1, n+1) {
+        sum += val
+        prod *= val
+        recip_sum = recip_sum + (1.0/val)
+    }
+    
+    a := sum / n
+    g := math.pow(prod, (1.0/n))
+    h := n / recip_sum
+    
+    result := 'Arithmetic Mean: ${a : 3.2f} \nGeometric Mean: ${g : 3.2f}\nHarmonic Mean: ${h : 3.2f}'
+    println(result)
+    
+    x := if a >= g && g >= h {"Yes"} else {"Nope"} 
+    println('Is A >= G >= H? $x')
+}
