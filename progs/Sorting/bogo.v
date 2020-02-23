1c8cfb39e4c5e3687ceb60aa52e54e02203005a3
adlesh <36427090+adlesh@users.noreply.github.com>
2020-02-22 23:24:20 -0500

    Added bogosort

---
+++ progs/Sorting/bogo.v
@@ -0,0 +1,31 @@
+import (
+	rand
+)
+
+fn shuffle(arr mut []int) {
+	for i := arr.len - 1; i >= 0; i-- {
+		j := rand.next(i + 1)
+		temp := arr[i]
+		arr[i] = arr[j]
+		arr[j] = temp
+	}
+	println('After Shuffle: ' + arr.str())
+}
+
+fn is_sorted(arr []int) bool {
+	for i := 0; i < arr.len; i++ {
+			return false
+		}
+	}
+	return true
+}
+
+fn main() {
+	rand.seed(100)
+	arr := [6, 9, 1, 4]
+	println('Input: $arr')
+	for !is_sorted(arr) {
+		shuffle(mut arr)
+	}
+	println('Output: $arr')
+}
