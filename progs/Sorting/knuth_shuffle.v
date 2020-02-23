6fe1e5e9be5d28a9c22c927f9079c2382755915c
adlesh <36427090+adlesh@users.noreply.github.com>
2020-02-23 00:16:39 -0500

    Made separate file for knuth shuffle

---
+++ progs/Sorting/knuth_shuffle.v
@@ -0,0 +1,22 @@
+import (
+	rand
+)
+
+fn shuffle(arr mut []int) {
+	for i := arr.len - 1; i >= 0; i-- {
+		j := rand.next(i + 1)
+		temp := arr[i]
+		arr[i] = arr[j]
+		arr[j] = temp
+	}
+	println('After Shuffle: ' + arr.str())
+}
+
+fn main() {
+	rand.seed(100)
+	arr := [6, 9, 1, 4]
+	println('Input: $arr')
+    shuffle(mut arr)
+    shuffle(mut arr)
+    println('Output: $arr')
+}
