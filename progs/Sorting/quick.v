b0adaf47856df5b49eac4f051b2d29cb09065ae7
adlesh <36427090+adlesh@users.noreply.github.com>
2020-02-26 20:14:29 -0500

    added simple quick sort
    remove println

---
+++ progs/Sorting/quick.v
@@ -0,0 +1,33 @@
+// rosettacode.org/wiki/Sorting_algorithms/Quicksort
+fn partition(arr mut []int, low, high int) int {
+	pivot := arr[high]
+	mut i := (low - 1)
+	for j in low .. high {
+		if arr[j] < pivot {
+			i++
+			temp := arr[i]
+			arr[i] = arr[j]
+			arr[j] = temp
+		}
+	}
+	temp := arr[i + 1]
+	arr[i + 1] = arr[high]
+	arr[high] = temp
+	return i + 1
+}
+
+fn quick_sort(arr mut []int, low, high int) {
+	if low < high {
+		pi := partition(mut arr, low, high)
+		quick_sort(mut arr, low, pi - 1)
+		quick_sort(mut arr, pi + 1, high)
+	}
+}
+
+fn main() {
+	mut arr := [4, 65, 2, -31, 0, 99, 2, 83, 782, 1]
+	n := arr.len - 1
+	println('Input: ' + arr.str())
+	quick_sort(mut arr, 0, n)
+	println('Output: ' + arr.str())
+}
