587d159291ab7103378de9f168e9dd3269c0a517
adlesh <36427090+adlesh@users.noreply.github.com>
2020-02-26 22:04:11 -0500

    added insertion sort

---
+++ progs/Sorting/insertion.v
@@ -0,0 +1,19 @@
+// http://rosettacode.org/wiki/Sorting_algorithms/Insertion_sort
+fn insertion(arr mut []int) {
+	for i in 1 .. arr.len {
+		value := arr[i]
+		mut j := i - 1
+		for j >= 0 && arr[j] > value {
+			arr[j + 1] = arr[j]
+			j--
+		}
+		arr[j + 1] = value
+	}
+}
+
+fn main() {
+	mut arr := [4, 65, 2, -31, 0, 99, 2, 83, 782, 1]
+	println('Input: ' + arr.str())
+	insertion(mut arr)
+	println('Output: ' + arr.str())
+}
